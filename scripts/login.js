30a6c0569ea352032744351d696c0558ad6d72de
riyadh435 <touhidulislamriyadh@gmail.com>
2025-02-25 23:31:22 +0600

    first commit

---
+++ scripts/login.js
@@ -0,0 +1,26 @@
+
+document.getElementById('numberInput').addEventListener('input', function () {
+    if (this.value.length > 0) {
+        this.classList.add('font-bold');
+        this.classList.add('text-black');
+        document.getElementById('labelOfinput').classList.remove("hidden");
+        
+    } else {
+        this.classList.remove('font-bold');
+        document.getElementById('labelOfinput').classList.add("hidden");
+    }
+});
+
+
+
+// click login button
+document.getElementById('btn-next').addEventListener('click', function () {
+    const inputField = document.getElementById("numberInput").value.trim();
+
+    if (inputField !== "") {
+        // Redirect to another file (change 'nextpage.html' to your actual file)
+        window.location.href = "../login/login1.html";
+    } else {
+        alert("Please fill in the input field before proceeding.");
+    }
+});
